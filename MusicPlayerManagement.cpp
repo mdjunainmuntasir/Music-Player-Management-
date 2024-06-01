b21ce24e1ac92c79adae876f5bc961841e1c954b
Md Junain Muntasir <junainmuntasir@gmail.com>
2024-05-31 21:37:43 -0400

    MusicPlayerManagement.cpp

--- MusicPlayerManagement.cpp
+++ MusicPlayerManagement.cpp
@@ -1,110 +1,326 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_set>
+#include <stdexcept>
+#include <limits>
+#include <algorithm>
+
 using namespace std;
 
-class temp{
-    string userName,Email,password;
-    string searchName,searchPass,searchEmail;
-    fstream file;
-    public:
-    void login();
-    void signUP();
-    void forgot();
-}obj;
+struct Node {
+    string songName;
+    string artist;
+    Node* prev;
+    Node* next;
+    vector<Node*> related;  // To store related songs (edges in a graph)
+};
 
-int main(){
-    char choice;
-    cout<<"\n1- Login";
-    cout<<"\n2- Sign-Up";
-    cout<<"\n3- Forgot Password";
-    cout<<"\n4- Exit";
-    cout<<"\nEnter Your Choice :: ";
-    cin>>choice;
+Node* createNode(const string& songName, const string& artist) {
+    Node* newNode = new Node;
+    newNode->songName = songName;
+    newNode->artist = artist;
+    newNode->prev = nullptr;
+    newNode->next = nullptr;
+    return newNode;
+}
 
-    switch(choice){
-        case '1':
-            cin.ignore();
-            obj.login();
-        break;
-        case '2':
-            cin.ignore();
-            obj.signUP();
-        break;
-        case '3':
-            cin.ignore();
-            obj.forgot();
-        break;
-        case '4':
-            return 0;
-        break;
-        defualt:
-            cout<<"Invalid Selection...!";
+void insertNode(Node** head, const string& songName, const string& artist) {
+    Node* newNode = createNode(songName, artist);
+    if (*head == nullptr) {
+        *head = newNode;
+    } else {
+        Node* temp = *head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
     }
 }
-void temp :: signUP(){
-    cout<<"\nEnter Your User Name :: ";
-    getline(cin,userName);
-    cout<<"Enter Your Email Address :: ";
-    getline(cin,Email);
-    cout<<"Enter Your Password :: ";
-    getline(cin,password);
 
-    file.open("loginData.txt",ios :: out | ios :: app);
-    file<<userName<<"*"<<Email<<"*"<<password<<endl;
-    file.close();
+void deleteNode(Node** head, const string& songName) {
+    Node* temp = *head;
+    while (temp != nullptr) {
+        if (temp->songName == songName) {
+            if (temp->prev != nullptr) {
+                temp->prev->next = temp->next;
+            }
+            if (temp->next != nullptr) {
+                temp->next->prev = temp->prev;
+            }
+            if (temp == *head) {
+                *head = temp->next;
+            }
+            delete temp;
+            cout << "Song '" << songName << "' deleted from the playlist." << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "Song '" << songName << "' not found in the playlist." << endl;
 }
-void temp :: login(){
- 
-    cout<<"----------LOGIN---------"<<endl;
-    cout<<"Enter Your User Name :: "<<endl;
-    getline(cin,searchName);
-    cout<<"Enter Your Password :: "<<endl;
-    getline(cin,searchPass);
 
-    file.open("loginData.txt",ios :: in);
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,password,'\n');
-    while(!file.eof()){
-        if(userName == searchName){
-            if(password == searchPass){
-                cout<<"\nAccount Login Succesfull...!";
-                cout<<"\nUsername :: "<<userName<<endl;
-                cout<<"\nEmail :: "<<Email<<endl;
-            }else{
-                cout<<"Password is Incorrect...!";
+void moveSong(Node** head, const string& songName) {
+    Node* temp = *head;
+    while (temp != nullptr && temp->songName != songName) {
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) {
+        cout << "Song '" << songName << "' not found in the playlist." << endl;
+        return;
+    }
+
+    if (temp == *head) {
+        cout << "Song '" << songName << "' is already at the beginning of the playlist." << endl;
+        return;
+    }
+
+    if (temp->prev != nullptr) {
+        temp->prev->next = temp->next;
+    }
+    if (temp->next != nullptr) {
+        temp->next->prev = temp->prev;
+    }
+    temp->prev = nullptr;
+    temp->next = *head;
+    (*head)->prev = temp;
+    *head = temp;
+
+    cout << "Song '" << songName << "' moved to the beginning of the playlist." << endl;
+}
+//this is the node for starting merge sort
+
+Node* merge(Node* left, Node* right) {
+    if (left == nullptr) return right;
+    if (right == nullptr) return left;
+
+    if (left->songName <= right->songName) {
+        left->next = merge(left->next, right);
+        left->next->prev = left;
+        left->prev = nullptr;
+        return left;
+    } else {
+        right->next = merge(left, right->next);
+        right->next->prev = right;
+        right->prev = nullptr;
+        return right;
+    }
+}
+
+void split(Node* source, Node** frontRef, Node** backRef) {
+    Node* fast = source->next;
+    Node* slow = source;
+
+    while (fast != nullptr) {
+        fast = fast->next;
+        if (fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
+
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = nullptr;
+}
+//this function is for merge sort implement
+void mergeSort(Node** head) {
+    if (*head == nullptr || (*head)->next == nullptr) return;
+
+    Node* a;
+    Node* b;
+
+    split(*head, &a, &b);
+
+    mergeSort(&a);
+    mergeSort(&b);
+
+    *head = merge(a, b);
+}
+//this is the main function calling for merge sort
+
+void sortPlaylist(Node** head) {
+    if (*head == nullptr) {
+        cout << "Playlist is empty." << endl;
+        return;
+    }
+
+    mergeSort(head);
+    cout << "Playlist sorted in alphabetical order." << endl;
+}
+
+void displayPlaylist(Node* head) {
+    if (head == nullptr) {
+        cout << "Playlist is empty." << endl;
+    } else {
+        cout << "Playlist:" << endl;
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << "Song: " << temp->songName << ", Artist: " << temp->artist << endl;
+            temp = temp->next;
+        }
+    }
+}
+
+void loadPlaylistFromFile(Node** head, const string& filename) {
+    ifstream file;
+    try {
+        file.open(filename);
+        if (!file) {
+            throw ios_base::failure("Error opening file");
+        }
+
+        string songName, artist;
+        while (getline(file, songName) && getline(file, artist)) {
+            insertNode(head, songName, artist);
+        }
+        file.close();
+    } catch (const ios_base::failure& e) {
+        cerr << "Exception opening/reading file: " << e.what() << endl;
+    }
+}
+
+void savePlaylistToFile(Node* head, const string& filename) {
+    ofstream file;
+    try {
+        file.open(filename);
+        if (!file) {
+            throw ios_base::failure("Error opening file");
+        }
+
+        Node* temp = head;
+        while (temp != nullptr) {
+            file << temp->songName << endl;
+            file << temp->artist << endl;
+            temp = temp->next;
+        }
+        file.close();
+    } catch (const ios_base::failure& e) {
+        cerr << "Exception opening/writing file: " << e.what() << endl;
+    }
+}
+
+void addRelatedSong(Node* song, Node* relatedSong) {
+    song->related.push_back(relatedSong);
+}
+
+void dfs(Node* start) {
+    if (!start) return;
+
+    unordered_set<Node*> visited;
+    stack<Node*> stack;
+    stack.push(start);
+
+    while (!stack.empty()) {
+        Node* current = stack.top();
+        stack.pop();
+
+        if (visited.find(current) == visited.end()) {
+            visited.insert(current);
+            cout << "Visited Song: " << current->songName << " by " << current->artist << endl;
+
+            for (Node* neighbor : current->related) {
+                if (visited.find(neighbor) == visited.end()) {
+                    stack.push(neighbor);
+                }
             }
         }
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,password,'\n');
     }
-    file.close();
 }
-void temp :: forgot(){
-    cout<<"\nEnter Your UserName :: ";
-    getline(cin,searchName);
-    cout<<"\nEnter Your Email Address :: ";
-    getline(cin,searchEmail);
 
-    file.open("loginData.txt",ios :: in);
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,password,'\n');
-    while(!file.eof()){
-        if(userName == searchName){
-            if(Email == searchEmail){
-                cout<<"\nAccount Found...!"<<endl;
-                cout<<"Your Password :: "<<password<<endl;
-            }else{
-                cout<<"Not found...!\n";
+int main() {
+    Node* playlist = nullptr;
+    const string filename = "playlist.txt";
+
+    // Load the playlist from the file at the beginning
+    loadPlaylistFromFile(&playlist, filename);
+
+    string songName, artist;
+    int choice;
+
+    while (true) {
+        cout << "\nMusic Playlist Management System" << endl;
+        cout << "1. Add Song" << endl;
+        cout << "2. Delete Song" << endl;
+        cout << "3. Reorder Song" << endl;
+        cout << "4. Sort Playlist" << endl;
+        cout << "5. Display Playlist" << endl;
+        cout << "6. Perform DFS Traversal" << endl;
+        cout << "7. Exit" << endl;
+        cout << "Enter your choice: ";
+
+        try {
+            if (!(cin >> choice)) {
+                throw invalid_argument("Invalid input. Please enter a number between 1 and 7.");
             }
-        }else{
-            cout<<"\nNot fount...!\n";
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the newline character left in the buffer
+
+            switch (choice) {
+                case 1:
+                    cout << "Enter song name: ";
+                    getline(cin, songName);
+                    cout << "Enter artist name: ";
+                    getline(cin, artist);
+                    insertNode(&playlist, songName, artist);
+                    savePlaylistToFile(playlist, filename);
+                    cout << "Song added to the playlist." << endl;
+                    break;
+                case 2:
+                    cout << "Enter song name to delete: ";
+                    getline(cin, songName);
+                    deleteNode(&playlist, songName);
+                    savePlaylistToFile(playlist, filename);
+                    break;
+                case 3:
+                    cout << "Enter song name to move to the beginning: ";
+                    getline(cin, songName);
+                    moveSong(&playlist, songName);
+                    savePlaylistToFile(playlist, filename);
+                    break;
+                case 4:
+                    sortPlaylist(&playlist);
+                    savePlaylistToFile(playlist, filename);
+                    break;
+                case 5:
+                    displayPlaylist(playlist);
+                    break;
+                case 6:
+                    cout << "Enter the song name to start DFS traversal: ";
+                    getline(cin, songName);
+                    {
+                        Node* temp = playlist;
+                        while (temp != nullptr && temp->songName != songName) {
+                            temp = temp->next;
+                        }
+                        if (temp != nullptr) {
+                            dfs(temp);
+                        } else {
+                            cout << "Song '" << songName << "' not found in the playlist." << endl;
+                        }
+                    }
+                    break;
+                case 7:
+                    // Free memory - cleanup before exiting
+                    while (playlist !=nullptr) {
+                        Node* temp = playlist;
+                        playlist = playlist->next;
+                        delete temp;
+                    }
+                    return 0;
+                default:
+                    cout << "Invalid choice. Please enter a number between 1 and 6." << endl;
+            }
+        } catch (const invalid_argument& e) {
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+            cout << e.what() << endl;
         }
-    getline(file,userName,'*');
-    getline(file,Email,'*');
-    getline(file,password,'\n');
     }
-    file.close();
+
+    return 0;
 }
